a4cf04d050591f4a5f906a84ba53a0b51daa61d9
Károly Török <torok.karoly.krisztian@gmail.com>
2020-12-27 12:26:34 +0100

    Initial import

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { h } from 'preact';
+import { useState, useEffect } from 'preact/hooks';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <section>
+        <div class="box">
+          <div class="circle">
+            <h2>
+              Happy <br /> Christmas
+            </h2>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default App;
